4041cbe09d935a575ddc4628b741a57ff5284060
Nisreen Salih <salihnisreen@gmail.com>
2021-02-10 02:28:10 +0000

    Update CoronavirusTrackerApplication.java

--- src/main/java/com/nso/coronavirustracker/CoronavirusTrackerApplication.java
+++ src/main/java/com/nso/coronavirustracker/CoronavirusTrackerApplication.java
@@ -2,11 +2,12 @@ package com.nso.coronavirustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class CoronavirusTrackerApplication {
+public class CoronavirusTrackerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CoronavirusTrackerApplication.class, args);
